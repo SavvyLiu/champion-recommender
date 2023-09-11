d96680ebef76c0a59d1e25db1e111ec96968a6cd
Eric Hayter <hayter.eric@gmail.com>
2023-09-10 21:54:15 -0400

    Co-authored-by: SavvyLiu <SavvyLiu@users.noreply.github.com>

--- format.py
+++ format.py
@@ -1,5 +1,5 @@
 import sqlite3
-import pickle
+import csv
 
 def main():
     # make a connection to the database 
@@ -12,16 +12,10 @@ def main():
     # make a query in mastery_data to find all the rows (entries) in mastery_data where puuid is the same (to a given target)
     # get the values from each of those rows (champion id and mastery) and make it into an array where the id is the index of the array
 
-    players = cur.execute('SELECT DISTINCT puuid FROM mastery_data').fetchall()
-    for player in players:
-        champs = cur.execute('SELECT championid, masterypoints FROM mastery_data WHERE puuid =?', player).fetchall()
-        mastery_points = {}
-        for champ, mastery in champs:
-            mastery_points[champ] = mastery
-        player_data.append(mastery_points)
-
-    with open('data.pickle', 'wb') as f:
-        pickle.dump(player_data, f, pickle.HIGHEST_PROTOCOL)
+    with open('data.csv', 'w', newline='') as f:
+        writer =  csv.writer(f)
+        players = cur.execute('SELECT puuid, championid, masterypoints FROM mastery_data').fetchall()
+        writer.writerows(players)
 
 if __name__ == '__main__':
     main()
